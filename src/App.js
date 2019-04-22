052f6ca95589ea60a95c16c75907d8774b3267b2
soiloof <tsucchy0718@gmail.com>
2019-04-22 15:47:43 +0900

    change src/app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-          <h1>Hello,world</h1>
-      </div>
+      <h1>hello,world!!</h1>
     );
   }
 }
